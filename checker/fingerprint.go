830f747deb6abac9ec99a5b50619ea64d3211679
Mathieu Deous <mat.deous@gmail.com>
2023-05-07 22:34:42 +0200

    :building_construction: use can-i-take-over-xyz as a submodule

--- checker/fingerprint.go
+++ checker/fingerprint.go
@@ -4,12 +4,11 @@ import (
 	_ "embed"
 	"encoding/json"
 	"github.com/mdeous/dnscheck/internal/log"
-	"io"
-	"net/http"
 	"os"
 )
 
-const FingerprintsUrl = "https://raw.githubusercontent.com/EdOverflow/can-i-take-over-xyz/master/fingerprints.json"
+//go:embed can-i-take-over-xyz/fingerprints.json
+var fpData []byte
 
 type Fingerprint struct {
 	CNames     []string `json:"cname"`
@@ -23,7 +22,6 @@ type Fingerprint struct {
 func LoadFingerprints(customFile string) []*Fingerprint {
 	//var data Data
 	var fingerprints []*Fingerprint
-	var fpData []byte
 	if customFile != "" {
 		// load fingerprints from user-provided file
 		log.Info("Loading fingerprints from %s", customFile)
@@ -32,18 +30,6 @@ func LoadFingerprints(customFile string) []*Fingerprint {
 			log.Fatal("Unable to read %s: %v", customFile, err)
 		}
 		fpData = content
-	} else {
-		// load fingerprints from can-i-take-over-xyz
-		log.Info("Loading fingerprints from can-i-take-over-xyz repository")
-		resp, err := http.Get(FingerprintsUrl)
-		if err != nil {
-			log.Fatal("Unable to fetch fingerprints: %v", err)
-		}
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal("Unable to read fingerprints: %v", err)
-		}
-		fpData = body
 	}
 	err := json.Unmarshal(fpData, &fingerprints)
 	if err != nil {
